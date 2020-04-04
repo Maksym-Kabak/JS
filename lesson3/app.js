11338bf98b38c793a5a24b79df87d52c64cbce78
Maksym-Kabak <maksitheslayer@gmail.com>
2020-04-04 13:11:01 +0300

    lesson4

--- lesson3/app.js
+++ lesson3/app.js
@@ -322,29 +322,29 @@
 // Частковий приклад реультату:
 // let usersWithCities = [{id: 1, name: 'vasya', age: 31, status: false, address: {user_id: 1, country: 'Ukraine', city: 'Ternopil'}}....]
 
-// let usersWithId = [
-//     {id: 1, name: 'vasya', age: 31, status: false},
-//     {id: 2, name: 'petya', age: 30, status: true},
-//     {id: 3, name: 'kolya', age: 29, status: true},
-//     {id: 4, name: 'olya', age: 28, status: false},
-// ];
-// let citiesWithId = [
-//     {user_id: 3, country: 'USA', city: 'Portland'},
-//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-//     {user_id: 2, country: 'Poland', city: 'Krakow'},
-//     {user_id: 4, country: 'USA', city: 'Miami'},
-// ];
-//
-// for (const user of usersWithId) {
-//     for (const cities of citiesWithId) {
-//         if (user.id === cities.user_id) {
-//             user.address = cities;
-//         }
-//     }
-// }
-// let usersWithCities = [];
-// usersWithCities.push(usersWithId);
-// console.log(usersWithCities);
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false},
+];
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'},
+];
+
+for (const user of usersWithId) {
+    for (const cities of citiesWithId) {
+        if (user.id === cities.user_id) {
+            user.address = cities;
+        }
+    }
+}
+let usersWithCities = [];
+usersWithCities.push(usersWithId);
+console.log(usersWithCities);
 
 // - створити розмітці блок з id, class та текстом в середені. Зчитати окремо цей текст з селекторів по id , class та тегу
 
